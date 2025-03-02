30f64ea33d8498a941049ad7db99750fd51dc285
vstlvl <zorix228337@gmail.com>
2025-03-02 16:24:06 +0300

    заменил переменные, хранящие данные, на ячейки в массиве статистики

--- enemy.py
+++ enemy.py
@@ -3,17 +3,19 @@ import random
 
 # дейдействия противника #
 def enemy_attack() -> bool:
-    if((random.randint(0, 100) / 100) < data.enemy_atack_chance):
-        data.player_hp -= data.enemy_damage
+    if((random.randint(0, 100) / 100) < data.enemy_stats[3]):
+        data.player_stats[1] -= data.enemy_stats[0]
         print("\nвраг атаковал")
         return True
     
 def enemy_defends(): 
-    if((random.randint(0, 100) / 100) < data.enemy_defends_chance):
-        data.enemy_hp += data.player_damage * data.enemy_absorbed_damage_proc
+    if((random.randint(0, 100) / 100) < data.enemy_stats[4]):
+        data.enemy_stats[1] += data.player_stats[0] * data.enemy_stats[2]
         print("враг защитился\n")
         return
     print("враг не защитился")
 
 def enemy_defeated():
-    data.player_balance += data.drops_money
+    data.player_stats[2] += data.enemy_stats[5]
+    
+def enemy_buff(): pass
--- player.py
+++ player.py
@@ -2,17 +2,17 @@ import data
 
 # действия игрока #
 def player_attack():
-    if(data.player_energy >= data.player_attack_energy_cost):
-        data.enemy_hp -= data.player_damage
-        data.player_energy -= data.player_attack_energy_cost
+    if(data.player_stats[4] >= data.player_stats[7]):
+        data.enemy_stats[1] -= data.player_stats[0]
+        data.player_stats[4] -= data.player_stats[7]
         print("\nвы атаковали")
     
     else: print("\nнедостаточно энергии")
     
 def player_defends():
-    if(data.player_energy >= data.player_defends_energy_cost):
-        data.player_hp += data.enemy_damage * data.player_absorbed_damage_proc
-        data.player_energy -= data.player_defends_energy_cost
+    if(data.player_stats[4] >= data.player_stats[6]):
+        data.player_stats[1] += data.enemy_stats[0] * data.player_stats[5]
+        data.player_stats[4] -= data.player_stats[6]
         print("вы защитились\n")
         
     else: print("недостаточно энергии")
